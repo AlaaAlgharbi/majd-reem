ae491e45f2ba40da1abc64a29cc50be28b699bbe
AlaaAlgharbi <algarbealaa@gmail.com>
2026-05-19 13:19:04 +0300

    add img

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -6,7 +6,6 @@ const RSVP = () => {
   const [ref, isVisible] = useIntersectionObserver({ threshold: 0.1 });
   const { t } = useTranslation();
 
-
   return (
     <div className="background">
       <div
@@ -16,6 +15,15 @@ const RSVP = () => {
         <h2 className={`hidden ${isVisible ? "fade-in-up delay-1" : ""}`}>
           {t("rsvp.rsvp")}
         </h2>
+        <div className={`hidden ${isVisible ? "fade-in-up delay-2" : ""}`}>
+          <a
+            href="https://www.facebook.com/share/g/1EASfKbWoh/?mibextid=wwXIfr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="rsvp-image" src="/img/4.jpg" alt="description" />
+          </a>
+        </div>
       </div>
     </div>
   );
