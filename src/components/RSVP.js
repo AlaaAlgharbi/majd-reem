a1f8fac28b421efa35deda125caeb127f7350de1
AlaaAlgharbi <algarbealaa@gmail.com>
2026-05-19 14:41:21 +0300

    add btn

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -15,13 +15,22 @@ const RSVP = () => {
         <h2 className={`hidden ${isVisible ? "fade-in-up delay-1" : ""}`}>
           {t("rsvp.rsvp")}
         </h2>
-        <div className={`hidden ${isVisible ? "fade-in-up delay-2" : ""}`}>
+        <div className={`hidden ${isVisible ? "fade-in-up delay-3" : ""}`}>
           <a
             href="https://www.facebook.com/share/g/1EASfKbWoh/?mibextid=wwXIfr"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img className="rsvp-image" src="/img/4.jpg" alt="description" />
+          </a>
+
+          <a
+            href="https://www.facebook.com/share/g/1EASfKbWoh/?mibextid=wwXIfr"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="btn btn-dark btn-sm px-1 py-1 "
+          >
+            Watch live
           </a>
         </div>
       </div>
